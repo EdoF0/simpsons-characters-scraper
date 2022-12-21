42aa8e5abfa44903778ce36fd156b81d52066235
Edoardo Fava <f.edoardo.0@gmail.com>
2022-12-21 14:40:32 +0100

    more exceptions

--- characterexeptions.py
+++ characterexeptions.py
@@ -3,4 +3,8 @@
 
 exceptions = {
     "https://simpsons.fandom.com/wiki/Abraham_The_Thief", # human error: relations inside other statuses
+    "https://simpsons.fandom.com/wiki/Aunt_Edith", # human error: gender and status swapped
+    "https://simpsons.fandom.com/wiki/Banker", # human error: status data error
+    "https://simpsons.fandom.com/wiki/Blue-eyed_wife", # syntax: gender and status images not inside a link
+    "https://simpsons.fandom.com/wiki/Brian_McGee", # human error: job is the fictional status image
 }
