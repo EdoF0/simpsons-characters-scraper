16ff9a939ba684d8fbf19d2c03c4dbc8c0806071
Edoardo Fava <f.edoardo.0@gmail.com>
2022-12-22 13:23:53 +0100

    more character exceptions

--- characterexeptions.py
+++ characterexeptions.py
@@ -7,4 +7,12 @@ exceptions = {
     "https://simpsons.fandom.com/wiki/Banker", # human error: status data error
     "https://simpsons.fandom.com/wiki/Blue-eyed_wife", # syntax: gender and status images not inside a link
     "https://simpsons.fandom.com/wiki/Brian_McGee", # human error: job is the fictional status image
+    "https://simpsons.fandom.com/wiki/Cocoa_Beanie", # human error: gender is the fictional status image
+    "https://simpsons.fandom.com/wiki/Dick_Tufeld", # semantic: he is a guest voice, not a character
+    "https://simpsons.fandom.com/wiki/Nuclear_Employee_1_(Who_Shot_Mr._Burns?)", # availability: response status 301 (moved permanently) then 404 (not found)
+    "https://simpsons.fandom.com/wiki/Nuclear_Employee_2_(Who_Shot_Mr._Burns?)", # availability: response status 301 (moved permanently) then 404 (not found)
+    "https://simpsons.fandom.com/wiki/Nuclear_Employee_3_(Who_Shot_Mr._Burns?)", # availability: response status 301 (moved permanently) then 404 (not found)
+    "https://simpsons.fandom.com/wiki/Pa_(How_Munched_Is_That_Birdie_in_the_Window?)", # availability: response status 301 (moved permanently) then 404 (not found)
+    "https://simpsons.fandom.com/wiki/Patrolman_1", # human error: status deceased image inside alias section
+    "https://simpsons.fandom.com/wiki/Vivienne_Saint_Germain", # human error: species and age inside other statuses
 }
