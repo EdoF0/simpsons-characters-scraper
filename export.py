586ee27a745de4099dcc8107885bc7e82a2b1f38
Edoardo Fava <f.edoardo.0@gmail.com>
2022-12-24 14:41:46 +0100

    csv export quote non numeric

--- export.py
+++ export.py
@@ -10,11 +10,13 @@ def writeCsv(data:list[dict], filename = CSV_FILE_NAME):
     # for newline argument check https://stackoverflow.com/questions/3191528/csv-in-python-adding-an-extra-carriage-return-on-windows
     with open(filename+"."+CSV_FILE_EXTENSION, 'w', encoding='utf-8' , newline='') as f:
         # set writer options
+        # https://docs.python.org/3/library/csv.html#csv-fmt-params
         writer = csv.DictWriter(f,
             fieldnames=data[0].keys(),
             delimiter=';',
             quotechar='"',
-            escapechar="\\"
+            escapechar="\\",
+            quoting=csv.QUOTE_NONNUMERIC, # in particular, quote all urls
         )
         # write header
         writer.writeheader()
